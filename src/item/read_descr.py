e69b08e8f920c327d2635725c05092f1cff11f92
aeon0 <aeon4@protonmail.com>
2023-10-31 11:53:22 +0100

    dont seperate each line

--- src/item/read_descr.py
+++ src/item/read_descr.py
@@ -32,28 +32,6 @@ def _closest_match(target, candidates, min_score=89):
     return None
 
 
-def _find_text_lines(img: np.ndarray):
-    if img is None or img.shape[0] == 0 or img.shape[1] == 0:
-        return []
-    res = []
-    pimg = cv2.Canny(img, 30, 150)
-    pimg = cv2.dilate(pimg, np.ones((1, 9)), iterations=1)
-    pimg = cv2.erode(pimg, np.ones((1, 9)), iterations=1)
-    cont2, _ = cv2.findContours(pimg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for c in cont2:
-        x2, y2, w2, h2 = cv2.boundingRect(c)
-        if 16 < w2 < 350 and 9 < h2 < 28:
-            roi = [
-                max(0, int(x2) - 7),
-                max(0, int(y2) - 6),
-                int(w2) + 14,
-                int(h2) + 12,
-            ]
-            res.append(roi)
-    res = sorted(res, key=lambda roi: roi[1])
-    return res
-
-
 def _find_number(s):
     matches = re.findall(r"[+-]?(\d+\.\d+|\.\d+|\d+\.?|\d+)\%?", s)
     if "Up to a 5%" in s:
@@ -70,6 +48,11 @@ def _clean_str(s):
     cleaned_str = re.sub(
         r"\((rogue|barbarian|druid|sorcerer|necromancer) only\)", "", cleaned_str
     )  # this is not included in our affix table
+    cleaned_str = re.sub(
+        r"(scroll up|account bound|requires level|sell value|durability|barbarian|rogue|sorceress|druid|necromancer)",
+        "",
+        cleaned_str,
+    )  # Remove new terms
     cleaned_str = " ".join(cleaned_str.split())  # Remove extra spaces
     return cleaned_str
 
@@ -80,7 +63,9 @@ def read_descr(rarity: ItemRarity, img_item_descr: np.ndarray) -> Item:
     # Detect textures (1)
     # =====================================
     refs = ["item_seperator_long", "item_seperator_long_2"]
-    if not (seperator_long := search(refs, img_item_descr, threshold=0.85, use_grayscale=True, mode="all")).success:
+    if not (
+        seperator_long := search(refs, img_item_descr, threshold=0.85, use_grayscale=True, mode="all", color_match="gray_seperator")
+    ).success:
         Logger.warning("Could not detect item_seperator_long.")
         screenshot("failed_seperator_long", img=img_item_descr)
         return None
@@ -91,7 +76,9 @@ def read_descr(rarity: ItemRarity, img_item_descr: np.ndarray) -> Item:
         x, y, w, h = match.region
         cv2.rectangle(masked_search_img, (x, y), (x + w, y + h), (0, 0, 0), -1)
     refs = ["item_seperator_short", "item_seperator_short_2"]
-    if not (seperator_short := search(refs, masked_search_img, threshold=0.75, use_grayscale=True, mode="best")).success:
+    if not (
+        seperator_short := search(refs, masked_search_img, threshold=0.68, use_grayscale=True, mode="best", color_match="gray_seperator")
+    ).success:
         Logger.warning("Could not detect item_seperator_short.")
         screenshot("failed_seperator_short", img=masked_search_img)
         return None
@@ -101,18 +88,15 @@ def read_descr(rarity: ItemRarity, img_item_descr: np.ndarray) -> Item:
     _, w, _ = img_item_descr.shape
     roi_top = [15, 15, w - 30, seperator_short.matches[0].center[1] - 20]
     crop_top = crop(img_item_descr, roi_top)
-    text_top = _find_text_lines(crop_top)
-    all_text = []
-    for t in text_top:
-        cropped_text = crop(crop_top, t)
-        r = image_to_text(cropped_text)
-        all_text.append(r.text)
-    concatenated_str = " ".join(all_text).lower()
+    concatenated_str = image_to_text(crop_top).text.lower().replace("\n", " ")
     if "item power" in concatenated_str:
         idx = concatenated_str.index("item power")
         preceding_word = concatenated_str[:idx].split()[-1]
         if preceding_word.isdigit():
             item.power = int(preceding_word)
+        elif "+" in preceding_word:
+            item_power_numbers = preceding_word.split("+")
+            item.power = int(item_power_numbers[0]) + int(item_power_numbers[1])
     max_length = 0
     for item_type in ItemType:
         if item_type.value in concatenated_str:
@@ -165,15 +149,7 @@ def read_descr(rarity: ItemRarity, img_item_descr: np.ndarray) -> Item:
         dy = next[1] - curr[1] + 5
         roi_full_affix = [curr[0] + 7, max(0, curr[1] - 16), w - 30 - curr[0], dy]
         img_full_affix = crop(img_item_descr, roi_full_affix)
-        text_affix = _find_text_lines(img_full_affix)
-        text_affix = sorted(text_affix, key=lambda roi: roi[1])  # sort by y coordinate
-        text_affix = [roi for roi in text_affix if roi[0] <= 20]  # filters out stuff like required level text on the right
-        all_text = []
-        for t in text_affix:
-            cropped_text = crop(img_full_affix, t)
-            r = image_to_text(cropped_text)
-            all_text.append(r.text)
-        concatenated_str = " ".join(all_text).lower()
+        concatenated_str = image_to_text(img_full_affix).text.lower().replace("\n", " ")
         cleaned_str = _clean_str(concatenated_str)
 
         found_key = _closest_match(cleaned_str, affix_dict)
@@ -197,15 +173,7 @@ def read_descr(rarity: ItemRarity, img_item_descr: np.ndarray) -> Item:
         dy = bottom_limit - ab[1]
         roi_full_aspect = [ab[0] + 7, max(0, ab[1] - 16), w - 30 - ab[0], dy]
         img_full_aspect = crop(img_item_descr, roi_full_aspect)
-        text_aspect = _find_text_lines(img_full_aspect)
-        text_aspect = sorted(text_aspect, key=lambda roi: roi[1])  # sort by y coordinate
-        text_aspect = [roi for roi in text_aspect if roi[0] <= 20]  # filters out stuff like required level text on the right
-        all_text = []
-        for t in text_aspect:
-            cropped_text = crop(img_full_aspect, t)
-            r = image_to_text(cropped_text)
-            all_text.append(r.text)
-        concatenated_str = " ".join(all_text).lower()
+        concatenated_str = image_to_text(img_full_aspect).text.lower().replace("\n", " ")
         cleaned_str = _clean_str(concatenated_str)
 
         found_key = _closest_match(cleaned_str, aspect_dict, min_score=77)

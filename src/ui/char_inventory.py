3760bfc9a3247ea43743c2ed1b60a22c35d8878e
Jo <aeon4@protonmail.com>
2023-11-07 09:23:56 +0100

    change inventory icon (#41)

--- src/ui/char_inventory.py
+++ src/ui/char_inventory.py
@@ -9,14 +9,9 @@ class CharInventory(InventoryBase):
         super().__init__()
         self.menu_name = "Char_Inventory"
         self.is_open_search_args: SearchArgs = SearchArgs(
-            ref=[
-                "char_inventory_active",
-                "char_inventory_active_hover",
-                "char_inventory_active_medium",
-                "char_inventory_active_hover_medium",
-            ],
+            ref=["sort_icon", "sort_icon_hover"],
             threshold=0.8,
-            roi=Config().ui_roi["character_active"],
+            roi=Config().ui_roi["sort_icon"],
             use_grayscale=False,
         )
         self.open_hotkey = Config().char["inventory"]

43c7acb76dcd62ee797eea90b2b941725c2c4008
Jo <aeon4@protonmail.com>
2023-11-11 13:21:34 +0100

    Update vision_mode.py

--- src/scripts/vision_mode.py
+++ src/scripts/vision_mode.py
@@ -124,13 +124,14 @@ def vision_mode():
                 if match:
                     canvas.config(highlightbackground="#23fc5d")
                     # Show matched bullets
-                    bullet_width = thick * 3
-                    for affix in item_descr.affixes:
-                        if affix.loc is not None and any(a == affix.type for a in matched_affixes):
-                            draw_rect(canvas, bullet_width, affix, off, "#23fc5d")
-
-                    if item_descr.aspect is not None and not did_match_affixes:
-                        draw_rect(canvas, bullet_width, item_descr.aspect, off, "#23fc5d")
+                    if item_descr is not None:
+                        bullet_width = thick * 3
+                        for affix in item_descr.affixes:
+                            if affix.loc is not None and any(a == affix.type for a in matched_affixes):
+                                draw_rect(canvas, bullet_width, affix, off, "#23fc5d")
+        
+                        if item_descr.aspect is not None and not did_match_affixes:
+                            draw_rect(canvas, bullet_width, item_descr.aspect, off, "#23fc5d")
                 elif not match:
                     canvas.config(highlightbackground="#fc2323")
 

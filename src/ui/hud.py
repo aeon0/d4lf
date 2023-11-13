96f3c26228b68ca470e7e60fc1c485bd2a114ae0
Jo <aeon4@protonmail.com>
2023-11-13 11:36:29 +0100

    Heal script (#64)

--- src/ui/hud.py
+++ src/ui/hud.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import math
 from cam import Cam
 from config import Config
 from utils.image_operations import color_filter, crop
@@ -18,8 +18,8 @@ class Hud(SearchArgs):
     def is_ingame(self, img: np.ndarray = None) -> bool:
         img = img if img is not None else Cam().grab()
         cropped_img = cv2.cvtColor(crop(img, Config().ui_roi["mini_map_visible"]), cv2.COLOR_BGR2GRAY)
-        _, binary_mask = cv2.threshold(cropped_img, 155, 255, cv2.THRESH_BINARY)
-        mini_map_visible = cv2.countNonZero(binary_mask) > 5
+        _, binary_mask = cv2.threshold(cropped_img, 145, 255, cv2.THRESH_BINARY)
+        mini_map_visible = cv2.countNonZero(binary_mask) > 20
         return self.is_visible(img=img) and mini_map_visible
 
     @staticmethod
@@ -55,3 +55,33 @@ class Hud(SearchArgs):
             if cd_sum > target_sum:
                 return True
         return False
+
+    @staticmethod
+    def get_health(img: np.ndarray = None) -> float:
+        img = Cam().grab() if img is None else img
+        cut_img = crop(img, Config().ui_roi["health_slice"])
+        sobel_y = cv2.Sobel(cut_img, cv2.CV_64F, 0, 1, ksize=3)
+        _, binary = cv2.threshold(np.abs(sobel_y), 100, 255, cv2.THRESH_BINARY)
+        blue, green, red = cv2.split(binary)
+        lines_red = cv2.HoughLinesP(red.astype(np.uint8), 1, math.pi / 2, 2, None, 30, 1)
+        lines_green = cv2.HoughLinesP(green.astype(np.uint8), 1, math.pi / 2, 2, None, 30, 1)
+        lines_blue = cv2.HoughLinesP(blue.astype(np.uint8), 1, math.pi / 2, 2, None, 30, 1)
+        lines = []
+        if lines_red is not None:
+            lines.extend(lines_red)
+        if lines_green is not None:
+            lines.extend(lines_green)
+        if lines_blue is not None:
+            lines.extend(lines_blue)
+        # for line in lines[0]:
+        #     pt1 = (line[0], line[1])
+        #     pt2 = (line[2], line[3])
+        #     draw_img = cut_img.copy()
+        #     cv2.line(draw_img, pt1, pt2, (0, 0, 255), 3)
+        max_health = 1.0
+        if len(lines) > 0:
+            max_health = 0.0
+            for line in lines[0]:
+                max_health = line[1] if line[1] > max_health else max_health
+        percentage = 1.0 - (max_health / cut_img.shape[0])
+        return percentage

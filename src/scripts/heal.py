3fb68792b2b68505236ac5a7a84afa6282db1650
aeon0 <aeon4@protonmail.com>
2023-11-13 11:27:18 +0100

    healing

--- src/scripts/heal.py
+++ src/scripts/heal.py
@@ -3,23 +3,24 @@ import traceback
 from cam import Cam
 from config import Config
 from ui.hud import Hud
+from ui.char_inventory import CharInventory
 from utils.misc import wait
 from logger import Logger
 
 
-def run_rogue_tb():
+def heal():
     hud = Hud()
-
-    Logger.info("Starting Rogue TB Script")
+    inv = CharInventory()
+    Logger.info("Starting Heal Script")
     while True:
         img = Cam().grab()
-        if hud.is_ingame(img):
-            ready = hud.is_skill_ready(img)
-            imbued = hud.is_imbued(img)
-            if ready and not imbued:
-                keyboard.send(Config().char["skill4"])
-                Logger.debug("Casting imbuement")
-        wait(0.1, 0.15)
+        if hud.is_ingame(img) and not inv.is_open():
+            health = hud.get_health(img)
+            if health < 0.8:
+                keyboard.send(Config().char["health_pot"])
+                Logger.debug(f"Healing {health}")
+
+        wait(0.3, 0.35)
 
 
 if __name__ == "__main__":
@@ -29,7 +30,7 @@ if __name__ == "__main__":
         start_detecting_window()
         while not Cam().is_offset_set():
             wait(0.2)
-        run_rogue_tb()
+        heal()
     except:
         traceback.print_exc()
         print("Press Enter to exit ...")

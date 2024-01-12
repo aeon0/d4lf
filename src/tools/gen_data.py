91a9e9e09d8731f6fa0dce5453101579e27e3234
aeon0 <aeon4@pm.me>
2024-01-12 05:21:24 -0800

    update data gen

--- src/tools/gen_data.py
+++ src/tools/gen_data.py
@@ -56,15 +56,16 @@ def main(d4data_dir: Path):
         for json_file in json_files:
             with open(json_file, "r", encoding="utf-8") as file:
                 data = json.load(file)
+                snoId = data["__snoID__"]
                 name_idx, desc_idx = (0, 1) if data["arStrings"][0]["szLabel"] == "Name" else (1, 0)
                 aspect_name = data["arStrings"][name_idx]["szText"]
                 aspect_name_clean = aspect_name.replace(" ", "_").lower().replace("’", "").replace("'", "")
                 aspect_name_clean = check_ms(aspect_name_clean)
                 aspect_desc = data["arStrings"][desc_idx]["szText"]
                 aspect_descr_clean = clean_str(remove_content_in_braces(aspect_desc.replace("’", "")))
-                aspects_dict[aspect_name_clean] = aspect_descr_clean
+                aspects_dict[aspect_name_clean] = {"desc": aspect_descr_clean, "snoId": snoId}
 
-        with open(f"assets/lang/{language}/aspects_{language}.json", "w", encoding="utf-8") as json_file:
+        with open(f"assets/lang/{language}/aspects.json", "w", encoding="utf-8") as json_file:
             json.dump(aspects_dict, json_file, indent=4, ensure_ascii=False)
             json_file.write("\n")
 
@@ -76,6 +77,7 @@ def main(d4data_dir: Path):
         for json_file in json_files:
             with open(json_file, "r", encoding="utf-8") as file:
                 data = json.load(file)
+                snoId = data["__snoID__"]
                 name_idx, _ = (0, 1) if data["arStrings"][0]["szLabel"] == "Name" else (1, 0)
                 name = data["arStrings"][name_idx]["szText"]
                 name_clean = name.replace(" ", "_").lower().replace("’", "").replace("'", "")
@@ -90,9 +92,9 @@ def main(d4data_dir: Path):
                     data = json.load(affix_file)
                     desc = data["arStrings"][0]["szText"]
                     desc_clean = clean_str(remove_content_in_braces(desc.replace("’", "")))
-                    unique_dict[name_clean] = desc_clean
+                    unique_dict[name_clean] = {"desc": desc_clean, "snoId": snoId}
 
-        with open(f"assets/lang/{language}/uniques_{language}.json", "w", encoding="utf-8") as json_file:
+        with open(f"assets/lang/{language}/uniques.json", "w", encoding="utf-8") as json_file:
             json.dump(unique_dict, json_file, indent=4, ensure_ascii=False)
             json_file.write("\n")
 
@@ -130,7 +132,7 @@ def main(d4data_dir: Path):
                             desc = sigil_affix["szText"].lower().replace("’", "").replace("'", "")
                         sigil_dict[affix_type][name.replace(" ", "_")] = f"{name} {clean_str(desc)}"
 
-        with open(f"assets/lang/{language}/sigils_{language}.json", "w", encoding="utf-8") as json_file:
+        with open(f"assets/lang/{language}/sigils.json", "w", encoding="utf-8") as json_file:
             json.dump(sigil_dict, json_file, indent=4, ensure_ascii=False)
             json_file.write("\n")
 
@@ -145,7 +147,7 @@ def main(d4data_dir: Path):
                 desc = clean_str(remove_content_in_braces(affix["szText"].replace("’", "").lower()))
                 if len(desc) > 2:
                     affix_dict[name] = desc
-        with open(f"assets/lang/{language}/affixes_{language}.json", "w", encoding="utf-8") as json_file:
+        with open(f"assets/lang/{language}/affixes.json", "w", encoding="utf-8") as json_file:
             json.dump(affix_dict, json_file, indent=4, ensure_ascii=False)
             json_file.write("\n")
 

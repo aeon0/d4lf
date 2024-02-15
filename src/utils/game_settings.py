ea40e467c6a8e868f1aceac739a01448b550a853
aeon0 <aeon4@pm.me>
2024-02-15 10:28:09 -0800

    dont error when localprefs.txt has no premission

--- src/utils/game_settings.py
+++ src/utils/game_settings.py
@@ -9,10 +9,17 @@ def is_fontsize_ok() -> bool:
     default_path = f"{user_dir}\\Documents\\Diablo IV/LocalPrefs.txt"
     file_path = custom_path if custom_path != "" else default_path
     if not os.path.exists(file_path):
-        Logger.warning("Could not find LocalPrefs.txt to check font size. Make sure you have font size set to small!")
+        Logger.warning(
+            "Could not find LocalPrefs.txt to check font size. Make sure you have font size set to small! If you have, ignore this warning."
+        )
         return True
-    with open(file_path, "r") as file:
-        file_content = file.read()
-        if 'FontScale "0"' not in file_content:
-            return False
+    try:
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            if 'FontScale "0"' not in file_content:
+                return False
+    except PermissionError:
+        Logger.warning(
+            "No premission to check the LocalPrefs.txt. Make sure you have font size set to small! If you have, ignore this warning."
+        )
     return True

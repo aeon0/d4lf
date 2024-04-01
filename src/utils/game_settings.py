853fa18b847f80829dc656e005ebc13d03868932
Jo <aeon4@protonmail.com>
2024-04-01 05:03:06 -0700

    update aspect, affixes, etc. to new patches (#194)

--- src/utils/game_settings.py
+++ src/utils/game_settings.py
@@ -10,9 +10,7 @@ def is_fontsize_ok() -> bool:
     default_path = f"{user_dir}\\Documents\\Diablo IV/LocalPrefs.txt"
     file_path = custom_path if custom_path != "" else default_path
     if not os.path.exists(file_path):
-        Logger.warning(
-            "Could not find LocalPrefs.txt to check font size. Make sure you have font size set to small! If you have, ignore this warning."
-        )
+        Logger.info("Could not find LocalPrefs.txt to check font size. Make sure you have font size set to small.")
         return True
     try:
         with open(file_path, "r") as file:
@@ -20,7 +18,5 @@ def is_fontsize_ok() -> bool:
             if 'FontScale "0"' not in file_content:
                 return False
     except PermissionError:
-        Logger.warning(
-            "No premission to check the LocalPrefs.txt. Make sure you have font size set to small! If you have, ignore this warning."
-        )
+        Logger.info("No premission to check the LocalPrefs.txt. Make sure you have font size set to small.")
     return True

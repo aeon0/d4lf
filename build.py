67cadf40699087fea6179b1055bcecccdd4a9371
chrisoro <4160557+chrisoro@users.noreply.github.com>
2024-05-28 09:59:48 +0200

    Add Release workflow (#251)

--- build.py
+++ build.py
@@ -1,60 +1,53 @@
 import argparse
-import getpass
 import os
 import shutil
+import site
 from pathlib import Path
 
 from cryptography.fernet import Fernet
 
 from src.version import __version__
 
-parser = argparse.ArgumentParser(description="Build d4lf")
-parser.add_argument(
-    "-c",
-    "--conda_path",
-    type=str,
-    help="Path to local conda e.g. C:\\Users\\USER\\miniconda3",
-    default=f"C:\\Users\\{getpass.getuser()}\\miniconda3",
-)
-parser.add_argument("-k", "--use_key", action="store_true", help="Will build with encryption key")
-args = parser.parse_args()
+EXE_NAME = "d4lf.exe"
+
+
+def build(use_key: bool, release_dir: Path):
+    clean_up()
+    if release_dir.exists():
+        shutil.rmtree(release_dir)
+    key_cmd = " "
+    if use_key:
+        key = Fernet.generate_key().decode("utf-8")
+        key_cmd = " --key " + key
+    installer_cmd = (
+        f"pyinstaller --clean --onefile --distpath {release_dir}{key_cmd} --paths .\\src --paths {site.getsitepackages()[1]} src\\main.py"
+    )
+    os.system(installer_cmd)
+    (release_dir / "main.exe").rename(release_dir / EXE_NAME)
 
 
 # clean up
 def clean_up():
     # pyinstaller
-    if Path("build").exists():
-        shutil.rmtree("build")
+    if (build_dir := Path("build")).exists():
+        shutil.rmtree(build_dir)
     if (p := Path("main.spec")).exists():
         p.unlink()
 
 
+def copy_additional_resources(release_dir: Path):
+    shutil.copy("README.md", release_dir)
+    shutil.copytree("assets", release_dir / "assets")
+    shutil.copytree("config", release_dir / "config")
+
+
 if __name__ == "__main__":
-    release_dir = Path(f"d4lf_v{__version__}")
+    PARSER = argparse.ArgumentParser(description="Build d4lf")
+    PARSER.add_argument("-k", "--use_key", action="store_true", help="Will build with encryption key")
+    ARGS = PARSER.parse_args()
+
     print(f"Building version: {__version__}")
+    RELEASE_DIR = Path(f"d4lf_v{__version__}")
 
-    clean_up()
-
-    if Path.exists(release_dir):
-        for path in Path(release_dir).glob("**/*"):
-            if path.is_file():
-                os.remove(path)
-            elif path.is_dir():
-                shutil.rmtree(path)
-        shutil.rmtree(release_dir)
-
-    for exe in ["main.py"]:
-        key_cmd = " "
-        if args.use_key:
-            key = Fernet.generate_key().decode("utf-8")
-            key_cmd = " --key " + key
-        installer_cmd = f"pyinstaller --onefile --distpath {release_dir}{key_cmd} --paths .\\src --paths {args.conda_path}\\envs\\d4lf\\Lib\\site-packages src\\{exe}"
-        os.system(installer_cmd)
-
-    os.system(f"cd {release_dir} && cd ..")
-
-    shutil.copy("README.md", f"{release_dir}/")
-    shutil.copytree("assets", f"{release_dir}/assets")
-    shutil.copytree("config", f"{release_dir}/config")
-    os.rename(f"{release_dir}/main.exe", f"{release_dir}/d4lf.exe")
-    clean_up()
+    build(use_key=ARGS.use_key, release_dir=RELEASE_DIR)
+    copy_additional_resources(RELEASE_DIR)

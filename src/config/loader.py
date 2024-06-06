7bd42160fe42486e76a90676d01b0a2e16223fd4
chrisoro <sloboda@gmx.de>
2024-06-06 21:59:34 +0200

    create empty params file if nothing is already there

--- src/config/loader.py
+++ src/config/loader.py
@@ -39,8 +39,12 @@ class IniConfigLoader:
         self._parsers["params"] = configparser.ConfigParser()
         self._parsers["custom"] = configparser.ConfigParser()
         self._parsers["params"].read(self._config_path / PARAMS_INI, encoding="utf-8")
-        if (p := (self.user_dir / PARAMS_INI)).exists() and p.stat().st_size:
-            self._parsers["custom"].read(p, encoding="utf-8")
+        if (p := (self.user_dir / PARAMS_INI)).exists():
+            if p.stat().st_size:
+                self._parsers["custom"].read(p, encoding="utf-8")
+        else:
+            with open(self.user_dir / PARAMS_INI, "w", encoding="utf-8"):
+                pass
 
         self._advanced_options = AdvancedOptionsModel(
             exit_key=self._select_val("advanced_options", "exit_key"),

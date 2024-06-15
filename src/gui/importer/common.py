ed50cc548fa30aa472f75a4f72fc69d8bd58c025
CJ Shrader <cjshrader@gmail.com>
2024-06-15 10:45:42 -0400

    Added User-Agent headers which should restore diablo.trade functionality (#295)
    Co-authored-by: chrisoro <sloboda@gmx.de>

--- src/gui/importer/common.py
+++ src/gui/importer/common.py
@@ -97,7 +97,10 @@ def get_class_name(input_str: str) -> str:
 
 def get_with_retry(url: str) -> requests.Response:
     for _ in range(5):
-        r = requests.get(url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+        }
+        r = requests.get(url, headers=headers)
         if r.status_code == 200:
             return r
         Logger.debug(f"Request {url} failed with status code {r.status_code}, retrying...")

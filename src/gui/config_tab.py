895ab1c8697a52673e3f3efd70327619f67c82d8
suchyindustries <ddiabloss@gmail.com>
2024-06-16 11:00:10 +0200

    Update config_tab.py

--- src/gui/config_tab.py
+++ src/gui/config_tab.py
@@ -164,11 +164,15 @@ class ConfigTab(QWidget):
         msg.setText(message)
         msg.setWindowTitle("Reset to default values?")
         msg.setStandardButtons(QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel)
-        if msg.exec():
+
+        result = msg.exec()  # Store the result of msg.exec()
+
+        if result == QMessageBox.StandardButton.Ok:
             IniConfigLoader().load(clear=True)
             self._reset_values_for_model(IniConfigLoader().general, "general")
             self._reset_values_for_model(IniConfigLoader().char, "char")
             self._reset_values_for_model(IniConfigLoader().advanced_options, "advanced_options")
+
 
     def _reset_values_for_model(self, model, section_config_header):
         for parameter in model:
